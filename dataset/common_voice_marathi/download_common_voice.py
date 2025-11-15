0c193aabd53dd3257f23be332bb18e1b53c89d63
Anamika Rajesh <84019126+Anamikarajesh@users.noreply.github.com>
2025-11-16 03:23:41 +0530

    Delete dataset/common_voice_marathi directory

--- dataset/common_voice_marathi/download_common_voice.py
+++
@@ -1,4 +0,0 @@
-from datasets import load_dataset
-dataset = load_dataset("mozilla-foundation/common_voice_17_0", "mr")
-dataset.save_to_disk("dataset/common_voice_marathi/local_copy")
-print("Marathi Common Voice dataset downloaded successfully!")
